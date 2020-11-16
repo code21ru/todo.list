0dea9f5bb22485a871d1a162ccd41bb6d0f470a4
Code21 <amirvulkan@gmail.com>
2020-11-16 22:26:56 +0300

    1. Added Csrf in todo.php

--- views/layouts/todo.php
+++ views/layouts/todo.php
@@ -11,10 +11,11 @@ $this->title = 'To-do List Code21';
 <!doctype html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
