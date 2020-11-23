43b1d2cd31983081cc0ffd6a875413175fe24b96
Путин <ploti@nalog.net>
2020-11-23 18:41:07 +0300

    1. Set default route in config web.php 2. Created views/content/index.php as a default view 3. Created ContentController.php to render content/index.php

--- config/web.php
+++ config/web.php
@@ -13,6 +13,7 @@ $config = [
     ],
     'layout' => 'todo',
     'language' => 'ru',
+    'defaultRoute' => 'content/index',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
